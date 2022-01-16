cf01e84dad25fae73a01a12caade06a459389583
tuncerrstm <tuncerrstm@gmail.com>
2022-01-17 01:59:33 +0300

    ADD : Project :airplane:

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,43 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(W => W.WriterName).NotEmpty().WithMessage("Ad Soyad Boş Geçilemez!");
+            RuleFor(W => W.WriterMail).NotEmpty().WithMessage("E-mail Boş Geçilemez!");
+            RuleFor(W => W.WriterPassword).NotEmpty().WithMessage("Şifre Boş Geçilemez!");
+            //RuleFor(W => W.WriterPasswordRepeat).NotEmpty().WithMessage("Şifre Tekrarı Boş Geçilemez!");
+            RuleFor(W => W.WriterName).MinimumLength(2).WithMessage("Minimum 2 karekter girişi yapınız!");
+            RuleFor(W => W.WriterName).MaximumLength(50).WithMessage("Maximum 50 karekter girişi yapınız!");
+            RuleFor(W => W.WriterImage).NotEmpty().WithMessage("Lütfen bir fotoğraf seçiniz!");
+            //RuleFor(w => w.WriterPassword).Must(IsPasswordValid).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakam olmalıdır!");
+            RuleFor(x => x.WriterPassword).MinimumLength(6).WithMessage("Parolanız en az 6 karakter içermelidir.");
+            RuleFor(x => x.WriterPassword).Must(IsPasswordValid).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakam içermelidir.");
+            RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Lütfen geçerli bir e-mail giriniz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkınızda kısmı boş Bırakılamaz.");
+        }
+
+        private bool IsPasswordValid(string arg)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[0-9])[A-Za-z\d]");
+                return regex.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,87 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<SelectListItem> countries = new List<SelectListItem>
+            {
+                 new SelectListItem
+                 {
+                     Text = "Bir Şehir Seçiniz.."
+                 },
+                new SelectListItem
+                 {
+                     Text = "Ankara",
+                     Value = 1.ToString()
+                 },
+                new SelectListItem
+                 {
+                     Text = "İstanbul",
+                     Value = 2.ToString()
+                 },
+                new SelectListItem
+                 {
+                     Text = "İzmir",
+                     Value = 3.ToString()
+                 },
+                new SelectListItem
+                 {
+                     Text = "Edirne",
+                     Value = 4.ToString()
+                 },
+                new SelectListItem
+                 {
+                     Text = "Bursa",
+                     Value = 5.ToString()
+                 },
+                new SelectListItem
+                 {
+                     Text = "Tekirdağ",
+                     Value = 6.ToString()
+                 }
+            }.ToList();
+            ViewBag.item = countries;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Test Page";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+    }
+}
